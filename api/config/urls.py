fc6c68454b621abb125631cdf851922afd7e0221
Sean Hayes <seanwhayes87@gmail.com>
2025-07-25 08:49:21 -0700

    Adding healthcheck

--- api/config/urls.py
+++ api/config/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from ninja import NinjaAPI
+from api.health_check import health_check
 from api.projects_controller import router as projects_router
 from api.services_controller import router as services_router
 
@@ -12,5 +13,6 @@ api.add_router("", services_router)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("healthcheck/", health_check, name="health_check"),
     path("api/", api.urls),
 ]
